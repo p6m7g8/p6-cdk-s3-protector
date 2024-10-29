0130f3fc796646ef233049615faf6f54b4bfc8b4
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-10-28 22:36:50 -0400

    chore: Initialize repository.

---
+++ src/p6cdks3protector.ts
@@ -0,0 +1,34 @@
+import type { Construct } from 'constructs'
+import * as cdk from 'aws-cdk-lib'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as lambdajs from 'aws-cdk-lib/aws-lambda-nodejs'
+import * as cr from 'aws-cdk-lib/custom-resources'
+import * as floyd from 'cdk-iam-floyd'
+
+export class P6CDKS3Protector extends cdk.Resource {
+  constructor(scope: Construct, id: string) {
+    super(scope, id)
+
+    const policy = new floyd.Statement.S3().allow().toPutObject().toPutObjectAcl()
+
+    const onEvent = new lambdajs.NodejsFunction(this, 'p6CDKS3Protector', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      timeout: cdk.Duration.seconds(5), // Adjust timeout if necessary
+      tracing: lambda.Tracing.ACTIVE,
+      bundling: {
+        minify: true,
+        externalModules: ['aws-sdk'],
+      },
+    })
+
+    onEvent.addToRolePolicy(policy)
+
+    const provider = new cr.Provider(this, 'P6CDKS3Protector/Provider', {
+      onEventHandler: onEvent,
+    })
+
+    new cdk.CustomResource(this, 'P6CDKS3Protector/CR', {
+      serviceToken: provider.serviceToken,
+    })
+  }
+}
---
+++ test/p6-cdk-s3-protector.test.ts
@@ -0,0 +1,21 @@
+import * as cdk from 'aws-cdk-lib'
+import { Template } from 'aws-cdk-lib/assertions'
+
+import { P6CDKS3Protector } from '../src'
+
+it('p6CDK3Protector components', () => {
+  // GIVEN
+  const app = new cdk.App()
+  const stack = new cdk.Stack(app, 'MyStack')
+
+  // WHEN
+  new P6CDKS3Protector(stack, 'p6-cdk-s3-protector')
+
+  // THEN
+  const template = Template.fromStack(stack)
+  template.hasResourceProperties('AWS::Lambda::Function', {
+    Handler: 'index.handler',
+    Runtime: 'nodejs20.x',
+  })
+  template.resourceCountIs('AWS::Lambda::Function', 2) // Custom Resource Handler counts too
+})
